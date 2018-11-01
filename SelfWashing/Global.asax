5643b65dc843442ec9fde6ed4d0c47f6b3c9ccd8
Alex Radu <alex_radu@live.com>
2018-11-01 10:49:26 +0200

    Add project files.

---
+++ SelfWashing/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SelfWashing.MvcApplication" Language="C#" %>
---
+++ SelfWashing/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SelfWashing.Startup))]
+namespace SelfWashing
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
